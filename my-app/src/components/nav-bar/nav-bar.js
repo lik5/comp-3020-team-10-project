a2774da3351b15e96886a065f4a595181386ac82
lik5 <143126314+lik5@users.noreply.github.com>
2024-11-23 13:09:43 -0600

    color scheme (#17)
    * styling changes
    
    * more styling

--- my-app/src/components/nav-bar/nav-bar.js
+++ my-app/src/components/nav-bar/nav-bar.js
@@ -1,10 +1,30 @@
 import './nav-bar.css';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 function NavBar() {
     let navigate = useNavigate();
-    const navigateTo = (path) => {
+    const url = window.location.href;
+
+    function getToggleState(url) {
+        const objInfo = url.split("http://localhost:3000/");
+        if (objInfo.length < 2) {
+            return 1;
+        } else if (objInfo[1].includes("cart")) {
+            return 3;
+        } else {
+            return 2;
+        }
+    }
+
+    const [toggle, setToggle] = useState(getToggleState(url));
+
+    function updateToggle(id) {
+        setToggle(id);
+    }
+    const navigateTo = (path, id) => {
         navigate("/"+path);
+        updateToggle(id);
     }
 
    return ( <div className="nav-bar">
@@ -13,9 +33,9 @@ function NavBar() {
             <input type="search" id="search-bar" name="searchbar" placeholder="Search the store"></input>
         </div>
         <div className='nav-tabs'>
-            <button onClick={() => navigateTo("")}>Home</button>
-            <button onClick={() => navigateTo("store")} >Products</button>
-            <button onClick={() => navigateTo("cart")}>Cart</button>
+            <button className= {toggle === 1 ? 'button active' : 'button'} onClick={() => navigateTo("", 1)}>Home</button>
+            <button className= {toggle === 2 ? 'button active' : 'button'} onClick={() => navigateTo("store/all", 2)} >Products</button>
+            <button className= {toggle === 3 ? 'button active' : 'button'} onClick={() => navigateTo("cart", 3)}>Cart</button>
         </div>
     </div>)
 }
