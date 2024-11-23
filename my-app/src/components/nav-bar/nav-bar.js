d0e6dcb288a49cdb0c45db545ec874b4b7ac48fa
lik5 <lik5@myumanitoba.ca>
2024-11-23 11:23:24 -0600

    styling changes

--- my-app/src/components/nav-bar/nav-bar.js
+++ my-app/src/components/nav-bar/nav-bar.js
@@ -1,10 +1,17 @@
 import './nav-bar.css';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 function NavBar() {
     let navigate = useNavigate();
-    const navigateTo = (path) => {
+    const [toggle, setToggle] = useState(1);
+
+    function updateToggle(id) {
+        setToggle(id);
+    }
+    const navigateTo = (path, id) => {
         navigate("/"+path);
+        updateToggle(id);
     }
 
    return ( <div className="nav-bar">
@@ -13,9 +20,9 @@ function NavBar() {
             <input type="search" id="search-bar" name="searchbar" placeholder="Search the store"></input>
         </div>
         <div className='nav-tabs'>
-            <button onClick={() => navigateTo("")}>Home</button>
-            <button onClick={() => navigateTo("store/all")} >Products</button>
-            <button onClick={() => navigateTo("cart")}>Cart</button>
+            <button className= {toggle === 1 ? 'button active' : 'button'} onClick={() => navigateTo("", 1)}>Home</button>
+            <button className= {toggle === 2 ? 'button active' : 'button'} onClick={() => navigateTo("store/all", 2)} >Products</button>
+            <button className= {toggle === 3 ? 'button active' : 'button'} onClick={() => navigateTo("cart", 3)}>Cart</button>
         </div>
     </div>)
 }
